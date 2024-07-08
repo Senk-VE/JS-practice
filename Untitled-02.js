1de0c6712a713773eb903dc6304d908074cae86b
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-08 16:42:47 +0300

    Update Untitled-02.js

--- Untitled-02.js
+++ Untitled-02.js
@@ -1,14 +1,21 @@
 const objectA = { a: 10, b: true };
 console.log(objectA);
+
+// изменение значения
 objectA.a = 20;
 console.log(objectA);
+
+//добавление свойства
 const copyOfA = objectA;
 copyOfA.c = 'white';
 console.log(copyOfA);
 
-function a() {
+//вывод функции a()
+function d() {
   console.log('hey there');
 }
-a();
-a = 10;
-a();
+d();
+
+//при замене значения переменной a, вызов функицц приведет к ошибке (a is not a function)
+// a = 10;
+// a();
