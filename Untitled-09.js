1df2d59e9983cb458f851a63eb80830e4e2395a1
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-12 14:28:13 +0300

    Update Untitled-09.js

--- Untitled-09.js
+++ Untitled-09.js
@@ -17,7 +17,25 @@ e = !!true; //true
 f = !!undefined; //false
 console.log(a, b, c, d, e);
 
-let x = !!{};
-console.log(x);
-console.log(typeof {});
-console.log(Boolean({}));
+console.log(Boolean({})); // пустой объект это true
+
+a1 = true && true; // t && t вернёт true
+a2 = true && false; // t && f вернёт false
+a3 = false && true; // f && t вернёт false
+a4 = false && 3 == 4; // f && f вернёт false
+a5 = 'Cat' && 'Dog'; // t && t вернёт "Dog"
+a6 = false && 'Cat'; // f && t вернёт false
+a7 = 'Cat' && false; // t && f вернёт false
+a8 = '' && false; // f && f вернёт ""
+a9 = false && ''; // f && f вернёт false
+
+o1 = true || true; // t || t вернёт true
+o2 = false || true; // f || t вернёт true
+o3 = true || false; // t || f вернёт true
+o4 = false || 3 == 4; // f || f вернёт false
+o5 = 'Cat' || 'Dog'; // t || t вернёт "Cat"
+o6 = false || 'Cat'; // f || t вернёт "Cat"
+o7 = 'Cat' || false; // t || f вернёт "Cat"
+o8 = '' || false; // f || f вернёт false
+o9 = false || ''; // f || f вернёт ""
+o10 = false || varObject; // f || object вернёт varObject
