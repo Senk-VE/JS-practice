5d8d5958601f769cb0253a8419b19d91b66c527d
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-13 23:25:43 +0300

    Create Untitled-12.js

---
+++ Untitled-12.js
@@ -0,0 +1,44 @@
+//Функциональные выражения
+const myFunction = function (a, b) {
+  let c;
+  a = a + 1;
+  c = a + b;
+  return c;
+};
+
+// myFunction(5, 3);
+console.log(myFunction(5, 3));
+
+setTimeout(function () {
+  console.log('hello world');
+}, 100);
+
+//Стрелочная функция
+
+setTimeout(() => {
+  console.log('here i am');
+}, 2000);
+
+//сокращенный вариант написания функции
+(a) => {
+  //тело фунции
+};
+
+(a, b) => a + b; //implicit return (неявное возвращение)
+
+const Mult = (value, multiplier = 1) => {
+  return value * multiplier;
+};
+console.log(Mult(7, 4)); //28
+
+//значение параметров функции по умолчанию
+
+const newPost = (post, addedAt = Date()) => ({
+  ...post,
+  addedAt,
+});
+const firstPost = {
+  id: 1,
+  aithor: 'Bogdan',
+};
+console.log(newPost(firstPost));
