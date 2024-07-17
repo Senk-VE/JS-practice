b1426707b9fb8bdf3e6193502dbe5f4b9bd64cc9
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-17 17:59:21 +0300

    Update Untitled-17.js

--- Untitled-17.js
+++ Untitled-17.js
@@ -40,3 +40,41 @@ console.log(love); // -10
 // }  else {
 //     //блок кода выполяется, если предыдущие условия ложны
 // }
+
+const sumPositiveNumbers = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'One of the arguments is not a number';
+  }
+  if (a <= 0 || b <= 0) {
+    return 'numbers are not positive';
+  }
+  return a + b;
+};
+console.log(sumPositiveNumbers('a', true)); //One of the arguments is not a number
+console.log(sumPositiveNumbers(-10, 5)); // numbers are not positive
+console.log(sumPositiveNumbers(3, 8)); // 11
+
+// // Инструкция switch. альтернатива инструкции if else if
+// switch (выражение) {
+//   case A:
+//     // действие если Выражение === A
+//     break;
+//   case B:
+//     // Действие если Выражение === B
+//     break;
+//   default:
+//   // Действие по умолчанию
+// }
+
+const month = 2; // Это не зимний месяц
+
+switch (month) {
+  case 12:
+    console.log('декабрь');
+    break; // это инструкция
+  case 1:
+    console.log('Январь');
+    break;
+  default:
+    console.log('Это не зимний месяц');
+}
