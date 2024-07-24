7d1498e6dd76580f41100fd6ca433d046210ec14
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-24 14:48:07 +0300

    Update Untitled-20a.mjs

--- modules/Untitled-20a.mjs
+++ modules/Untitled-20a.mjs
@@ -1,3 +1,10 @@
 const sum = (a, b) => a + b;
 
-export default sum;
+const one = 1;
+const two = 'two';
+
+const mult = (a, b) => a * b;
+
+// export default sum; когда добавился вызов двух функий, то вместо 'default' используем переменные в {}
+export { one, two };
+export { sum, mult };
