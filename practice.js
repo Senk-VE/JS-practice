b0be52b933bf1d9e173f9df9eafb02441ac5d63f
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-25 13:15:49 +0300

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,7 @@
+// .charAt(0) используется для получения символа строки по указанному индексу
+// .substring(1) используется для извлечения подстроки из исходной строки
+// в итоге получаем первый символ строки заглавным
+const capitalized = (word) => {
+  return word.charAt(0).toUpperCase() + word.substring(1);
+};
+console.log(capitalized('steps'));
