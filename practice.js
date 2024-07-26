0d04512c79579a38c984ac692da799a9c37ffeb5
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-26 20:00:45 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -19,22 +19,34 @@ console.log(b);
 let str = 'Hello, World!'; // str.substring(indexStart, indexEnd) // Если indexStart больше indexEnd, substring меняет их местами.
 console.log(str.substring(7, 12)); // "World"
 console.log(str.substring(7)); // "World!" // Если indexEnd не указан, извлечение продолжается до конца строки.
-
 let str = 'Hello, World!'; // str.substr(start, length)
 console.log(str.substr(7, 5)); // "World"
 console.log(str.substr(7)); // "World!" // Если length не указан, извлечение продолжается до конца строки
 
-// замена элемента массива, где первый элемент массива имеет индекс 0
+// Оператор %
+// Проверка на четность
+let number = 7;
+if (number % 2 === 0) {
+  console.log('Число четное');
+} else {
+  console.log('Число нечетное');
+}
+// Остаток
+let dividend = 10;
+let divisor = 3;
+let remainder = dividend % divisor;
+console.log(remainder); // Выведет 1
 
+// Массив
+// замена элемента массива, где первый элемент массива имеет индекс 0
 const myArray = ['abbc', true, 846];
 console.log(myArray);
-
 myArray[1] = false;
 console.log(myArray);
 
 // метод массива splice
 let array = [1, 2, 3, 4, 5];
-array.splice(array.length, 0, 6, 7);
+array.splice(array.length, 0, 6, 7); // (start, deleteCount, item1, item2, ...)
 console.log(array); // [1, 2, 3, 4, 5, 6, 7]
 
 // перебор массива forEach
