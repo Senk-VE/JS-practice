83b77202e46d262ba82d78686f717eda55ecb1ef
Senk-VE <154221541+Senk-VE@users.noreply.github.com>
2024-07-29 21:37:34 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -79,3 +79,25 @@ const objectKeys = Object.keys(myObject)
 objectKeys.forEach((key) => {
   if (key === 'key1' || key === 'key3') {
     console.log(myObject[key])  }})
+
+// Генератор случайных чисел
+//
+const MIN = 1000
+const MAX = 9999
+const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351]
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const addRandomNumberToArray = (arr, min, max) => {
+  let newRandomNumber
+  const updatedArr = [...arr]
+  do {
+    newRandomNumber = randomNumber(min, max)
+    // console.log(newRandomNumber)
+  } while (updatedArr.includes(newRandomNumber))
+  updatedArr.push(newRandomNumber)
+  return updatedArr
+}
+const updatedArray = addRandomNumberToArray(myNumbers, MIN, MAX)
+console.log('UPDATED ARRAY', updatedArray)
+console.log('ORIGINAL ARRAY', myNumbers)
